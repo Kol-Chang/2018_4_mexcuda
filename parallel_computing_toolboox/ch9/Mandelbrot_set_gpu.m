92210008f82d54035fc2a3da46973c27ffe7d3a9
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-11 15:47:49 -0700

    changes

--- parallel_computing_toolboox/ch9/Mandelbrot_set_gpu.m
+++ parallel_computing_toolboox/ch9/Mandelbrot_set_gpu.m
@@ -83,4 +83,15 @@ title( sprintf( '%1.3fsecs (GPU arrayfun) = %1.1fx faster', ...
 fprintf( '%1.3fsecs (GPU arrayfun) = %1.1fx faster\n', ...
     gpuArrayfunTime, cpuTime/gpuArrayfunTime )
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function count = pctdemo_processMandelbrotElement(x0,y0,maxIterations)
+z0 = complex(x0,y0);
+z = z0;
+count = 1;
+while (count <= maxIterations) && (abs(z) <= 2)
+    count = count + 1;
+    z = z*z + z0;
+end
+count = log(count);
+
 
