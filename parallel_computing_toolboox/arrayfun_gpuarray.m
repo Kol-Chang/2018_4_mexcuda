94f895fe9cb94c7112da32471d35168c32ade180
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-11 19:41:39 -0700

    changes

--- parallel_computing_toolboox/arrayfun_gpuarray.m
+++ parallel_computing_toolboox/arrayfun_gpuarray.m
@@ -2,8 +2,8 @@
 
 hornerFcn = @horner;
 
-data1  = rand( 2000, 'single' );
-data2  = rand( 1000, 'double' );
+data1  = rand( 4000, 'single' );
+data2  = rand( 2000, 'double' );
 gdata1 = gpuArray( data1 );
 gdata2 = gpuArray( data2 );
 
