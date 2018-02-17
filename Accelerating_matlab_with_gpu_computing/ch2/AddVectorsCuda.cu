7dadb33d4d49f694e509486530777ef01adfac30
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-16 19:44:20 -0700

    changes

--- Accelerating_matlab_with_gpu_computing/ch2/AddVectorsCuda.cu
+++ Accelerating_matlab_with_gpu_computing/ch2/AddVectorsCuda.cu
@@ -1,6 +1,34 @@
 #include "mex.h"
-#include "AddVectors.h"
 #include "cuda_runtime.h"
+
+__global__  void addVectorsMask(float* A, float*B, float*C, int size)
+{
+	int i = blockIdx.x;
+	if(i >= size)
+		return;
+
+	C[i] = A[i] + B[i];
+}
+
+void addVectors(float* A, float* B, float* C, int size)
+{
+	float *devPtrA = 0, *devPtrB = 0, *devPtrC = 0;
+
+	cudaMalloc(&devPtrA, sizeof(float)*size);
+	cudaMalloc(&devPtrB, sizeof(float)*size);
+	cudaMalloc(&devPtrC, sizeof(float)*size);
+
+	cudaMemcpy(devPtrA, A, sizeof(float)*size, cudaMemcpyHostToDevice);
+	cudaMemcpy(devPtrB, B, sizeof(float)*size, cudaMemcpyHostToDevice);
+
+	addVectorsMask<<<size, 1>>>(devPtrA, devPtrB, devPtrC, size);
+
+	cudaMemcpy(C, devPtrC, sizeof(float)*size, cudaMemcpyDeviceToHost);
+
+	cudaFree(devPtrA);
+	cudaFree(devPtrB);
+	cudaFree(devPtrC);
+}
 
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, mxArray* prhs[])
 {	
