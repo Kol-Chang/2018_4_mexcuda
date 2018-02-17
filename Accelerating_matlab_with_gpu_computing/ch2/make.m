ddcbc7bd7eb85dbc2af89617abae1f2cdfc97e26
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-16 20:08:43 -0700

    changes

--- Accelerating_matlab_with_gpu_computing/ch2/make.m
+++ Accelerating_matlab_with_gpu_computing/ch2/make.m
@@ -1,3 +1,3 @@
 %/cm/shared/apps/cuda80/toolkit/8.0.61/lib64
 
-mexcuda '-L/cm/shared/apps/cuda80/toolkit/8.0.61/lib64' -lcudart  AddVectorsCuda.cpp AddVectors.cu
+mexcuda AddVectorsCuda.cpp AddVectors.cu
