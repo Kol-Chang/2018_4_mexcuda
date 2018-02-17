92b6cb4092a5bdfd8f8a67713bdb50e86d5a7839
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-02-16 20:13:14 -0700

    changes

--- Accelerating_matlab_with_gpu_computing/ch2/AddVectors.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ADDVECTORS_H_
-#define __ADDVECTORS_H_
-
-extern void addVectors(float*A, float*B, float*C, int size);
-
-#endif
--- Accelerating_matlab_with_gpu_computing/ch2/make.m
+++
@@ -1,3 +0,0 @@
-%/cm/shared/apps/cuda80/toolkit/8.0.61/lib64
-
-mexcuda AddVectorsCuda.cpp AddVectors.cu
